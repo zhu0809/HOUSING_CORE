950772612d2a386cf22324fc3c4662452c14ed53
zhurongguo <15761676362@163.com>
2018-12-19 11:20:38 +0800

    修改部分核心配置

--- src/main/java/com/bs/housing/utils/WebUtils.java
+++ src/main/java/com/bs/housing/utils/WebUtils.java
@@ -2,7 +2,6 @@ package com.bs.housing.utils;
 
 import com.bs.housing.App;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 /**
@@ -13,11 +12,12 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 public interface WebUtils {
     /*返回数据*/
     String RESULT = "result";
-    View VIEW = new MappingJackson2JsonView();
+    MappingJackson2JsonView VIEW = new MappingJackson2JsonView();
     String MESSAGE = "message";
     String DATA = "data";
     String STATUS = "status";
     /*返回数据 end*/
+    
     /*全局数据核心配置*/
     ConfigurableApplicationContext CONTEXT = App.CONTEXT;
 }
